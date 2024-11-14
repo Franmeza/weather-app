7c1853be059ccf7be770f27cbc668a3c099d0ed6
FranMeza <ing.meza.francisco@gmail.com>
2024-11-14 14:57:23 -0700

    created header containing a search bar and temperature unit switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <div className=" flex px-[72px] py-7 ">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import SearchIcon from "@/assets/Search.svg";
+import { useState } from "react";
+
+function Header() {
+  const [tempUnit, setTempUnit] = useState("C");
+
+  const handleTempUnit = (unit: string) => {
+    setTempUnit(unit);
+  };
+  return (
+    <header className="flex w-full items-center justify-between ">
+      <div className=" relative">
+        <input
+          className="rounded-full pl-11 py-2 bg-[#20293A] border border-[#20293A] focus:border-[#030616] focus:outline-none"
+          type="text"
+          placeholder="Search city..."
+        />
+        <span className="flex items-center absolute top-0 inset-y-0 ml-2  mr-4">
+          <img src={SearchIcon} alt="magnifier" />
+        </span>
+      </div>
+      <div className="bg-[#20293A] rounded-full p-1 text-white font-bold">
+        <button
+          className={`${
+            tempUnit === "C" ? "bg-white text-[#20293A]" : "bg-none"
+          } rounded-full h-8 w-8 `}
+          onClick={() => handleTempUnit("C")}
+        >
+          °C
+        </button>
+        <button
+          className={`${
+            tempUnit === "F" ? " bg-white text-[#20293A]" : "bg-none"
+          } rounded-full h-8 w-8  `}
+          onClick={() => handleTempUnit("F")}
+        >
+          °F
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
