a2b617075b54743cdd3d61dc4e8385fc3441ce9e
FranMeza <ing.meza.francisco@gmail.com>
2024-11-29 13:33:21 -0700

    changed grid layaout for viewports smaller than 800px

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="px-[72px] py-7 ">
+    <div className="px-6 lg:px-8 xl:px-[72px] py-7 ">
       <Header />
-      <div className="grid grid-cols-[auto,1fr]  gap-x-11 gap-y-9 mt-4">
-        <CurrentLocation />
+      <div className="grid min-[800px]:grid-cols-[auto,1fr]  gap-x-11 gap-y-9 mt-4">
+        <div>
+          <CurrentLocation />
+        </div>
         <TodayForecast />
         <OtherCitiesWeather />
         <FiveDaysForecast />
--- src/utils/localTime.ts
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-
-export const localTime = new Intl.DateTimeFormat("default", {
-  hour: "2-digit",
-  minute: "2-digit",
-}).format(currentDate);
