3ff11dc86f95a9cd89f74401ce50bdab0f5f38ea
Franmeza <ing.meza.francisco@gmail.com>
2024-12-10 16:19:11 -0700

    added Five days forecast section

--- src/components/FiveDaysForecast.tsx
+++ src/components/FiveDaysForecast.tsx
@@ -1,10 +1,81 @@
+import { useWeatherContext } from "@/hooks/useWeatherContext";
 import H2 from "./reusable/H2";
+import { format } from "date-fns";
+
+import unitTempConvert from "@/utils/tempConversor";
+import TemperatureBar from "./TemperatureBar";
+
+type DailyForecast = {
+  temp: number;
+  temp_min: number;
+  temp_max: number;
+  weather: {
+    icon: string;
+    main: string;
+  };
+  date: number;
+};
 
 function FiveDaysForecast() {
+  const { weatherForecast, tempUnit } = useWeatherContext();
+  if (!weatherForecast) return null;
+
+  const dailyForcast = weatherForecast.list.reduce((acc, forecast) => {
+    const date = format(new Date(forecast.dt * 1000), "yyyy-MM-dd");
+
+    if (!acc[date]) {
+      acc[date] = {
+        temp: unitTempConvert(forecast.main.temp, tempUnit),
+        temp_min: unitTempConvert(forecast.main.temp_min, tempUnit),
+        temp_max: unitTempConvert(forecast.main.temp_max, tempUnit),
+        weather: forecast.weather[0],
+        date: forecast.dt,
+      };
+    } else {
+      acc[date].temp_min = Math.min(
+        acc[date].temp_min,
+        unitTempConvert(forecast.main.temp_min, tempUnit)
+      );
+      acc[date].temp_max = Math.max(
+        acc[date].temp_max,
+        unitTempConvert(forecast.main.temp_max, tempUnit)
+      );
+    }
+
+    return acc;
+  }, {} as Record<string, DailyForecast>);
+
+  const nextDays = Object.values(dailyForcast).slice(0, 5);
+  console.log(nextDays);
+
   return (
-    <section className="flex flex-col gap-4  border border-white ">
+    <section className="space-y-5">
       <H2>5-day forecast</H2>
-      <div className="border border-white h-[118px]"></div>
+      <div className="flex flex-col gap-3">
+        {nextDays.map((day) => (
+          <div
+            key={day.date}
+            className="flex items-center justify-between gap-[40px]  lg:flex-nowrap lg:gap-[77px] bg-[#20293A] rounded-xl px-5 py-3"
+          >
+            <p>{format(new Date(day.date * 1000), "EEE")}</p>
+
+            <div className="flex justify-center items-center ">
+              <img
+                src={`https://openweathermap.org/img/wn/${day.weather.icon}@4x.png`}
+                width={36}
+                height={36}
+              />
+              <p className="text-[10px] text-[#97A3B6]">{day.weather.main}</p>
+            </div>
+
+            <TemperatureBar
+              currentTemp={day.temp}
+              maxTemp={day.temp_max}
+              minTemp={day.temp_min}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/components/TemperatureBar.tsx
@@ -0,0 +1,27 @@
+function TemperatureBar({ currentTemp, minTemp, maxTemp }) {
+  const range = Math.abs(maxTemp - minTemp);
+
+  return (
+    <div className="flex w-full max-w-sm items-center  gap-3">
+      <span>{minTemp}°</span>
+      <div className="relative h-[6px] w-full rounded-full  bg-[#030616]">
+        <div
+          className="absolute h-full rounded-full bg-blue-500 "
+          style={{
+            width: `${range}%`,
+            left: `${Math.max(
+              0,
+              Math.min(
+                100 - 10,
+                ((currentTemp - minTemp) / range) * 100 - 10 / 2
+              )
+            )}%`,
+          }}
+        ></div>
+      </div>
+      <span>{maxTemp}°</span>
+    </div>
+  );
+}
+
+export default TemperatureBar;
